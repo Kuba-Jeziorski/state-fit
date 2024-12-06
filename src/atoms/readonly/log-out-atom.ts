5564d180aaf3ae103e31cbd02f48f46534504c15
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2024-12-06 18:21:01 +0000

    logOut atom

---
+++ src/atoms/readonly/log-out-atom.ts
@@ -0,0 +1,17 @@
+import { atom } from "jotai";
+import { currentTrainingIdAtom } from "../current-training-id-atom";
+import { trainingsAtom } from "../trainings-atom";
+import { exercisesAtom } from "../exercises-atom";
+import { exerciseSetsAtom } from "../exercise-sets-atom";
+import { TOKEN_NOT_PROVIDED, TRAINING_OFF } from "../../constants/constants";
+import { tokenAtom } from "./token-atom";
+import { trainingStateAtom } from "../training-state-atom";
+
+export const logOutAtom = atom(null, (_, set) => {
+  set(tokenAtom, TOKEN_NOT_PROVIDED);
+  set(trainingStateAtom, TRAINING_OFF);
+  set(currentTrainingIdAtom, null);
+  set(trainingsAtom, {});
+  set(exercisesAtom, {});
+  set(exerciseSetsAtom, {});
+});
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,6 @@ import {
   LOGGING_CONFIRM_MESSAGE,
   NEW_TRAINING_CONFIRM_MESSAGE,
   START_NEW_TRAINING_CAPTION,
-  TOKEN_NOT_PROVIDED,
-  TRAINING_OFF,
   TRAINING_ON,
 } from "../constants/constants";
 
@@ -16,16 +14,12 @@ import { usePageTitle } from "../utils/usePageTitle";
 import { useRedirectToSummary } from "../utils/useRedirectToSummary";
 import { useRedirectToTraining } from "../utils/useRedirectToTraining";
 import { Button } from "../components/Button";
-import { currentTrainingIdAtom } from "../atoms/current-training-id-atom";
-import { useAtom, useSetAtom } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
 import { trainingStateAtom } from "../atoms/training-state-atom";
-import { exercisesAtom } from "../atoms/exercises-atom";
-import { exerciseSetsAtom } from "../atoms/exercise-sets-atom";
-import { trainingsAtom } from "../atoms/trainings-atom";
-import { tokenAtom } from "../atoms/readonly/token-atom";
 import { startTrainingAtom } from "../atoms/writeonly/start-training-atom";
 
 import axios from "axios";
+import { logOutAtom } from "../atoms/readonly/log-out-atom";
 
 const FetchButton = () => {
   const [id, setId] = useState(0);
@@ -54,24 +48,21 @@ export const Home = () => {
   const [isLogoutPressed, setIsLoggoutPressed] = useState(false);
   const [isTrainingPressed, setIsTrainingPressed] = useState(false);
 
-  const [trainingStateValue, setTraningState] = useAtom(trainingStateAtom);
+  const trainingStateValue = useAtomValue(trainingStateAtom);
 
-  const setCurrentTrainingId = useSetAtom(currentTrainingIdAtom);
-  const setTrainings = useSetAtom(trainingsAtom);
-  const setExercises = useSetAtom(exercisesAtom);
-  const setExerciseSets = useSetAtom(exerciseSetsAtom);
-  const setToken = useSetAtom(tokenAtom);
+  const logOut = useSetAtom(logOutAtom);
+  const setTraningState = useSetAtom(trainingStateAtom);
+  const setStartTraining = useSetAtom(startTrainingAtom);
 
-  const startTraining = useSetAtom(startTrainingAtom);
+  const redirectToTraining = useRedirectToTraining();
+  const redirectToSummary = useRedirectToSummary();
+  usePageTitle(HOME_PAGE_TITLE);
 
   const isTrainingOn = trainingStateValue === TRAINING_ON;
 
   const trainingButtonCaption = isTrainingOn
     ? CURRENT_TRAINING_CAPTION
     : START_NEW_TRAINING_CAPTION;
-
-  const redirectToTraining = useRedirectToTraining();
-  const redirectToSummary = useRedirectToSummary();
 
   const handleIsTrainingPressed = () => {
     if (isTrainingOn) {
@@ -82,7 +73,7 @@ export const Home = () => {
   };
   const trainingConfirmAccepted = () => {
     setTraningState(TRAINING_ON);
-    startTraining();
+    setStartTraining();
 
     setIsTrainingPressed(false);
     redirectToTraining();
@@ -94,19 +85,12 @@ export const Home = () => {
     setIsLoggoutPressed(true);
   };
   const logOutConfirmAccepted = () => {
-    setToken(TOKEN_NOT_PROVIDED);
-    setTraningState(TRAINING_OFF);
+    logOut();
     setIsLoggoutPressed(false);
-    setCurrentTrainingId(null);
-    setTrainings({});
-    setExercises({});
-    setExerciseSets({});
   };
   const logOutConfirmDeclined = () => {
     setIsLoggoutPressed(false);
   };
-
-  usePageTitle(HOME_PAGE_TITLE);
 
   return (
     <>
